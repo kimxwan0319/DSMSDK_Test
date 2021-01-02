b0fbc0748393ba48f1fa27a0b6ff3a7abdd8a1ea
kimxwan0319 <kimxwan0319@naver.com>
2021-01-03 00:26:03 +0900

    Initial commit

--- DSM_AuthTest/Pods/Target Support Files/Pods-DSM_AuthTest/Pods-DSM_AuthTest-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DSM_AuthTest : NSObject
-@end
-@implementation PodsDummy_Pods_DSM_AuthTest
-@end
---
+++ testSDK/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  testSDK
+//
+//  Created by 김수완 on 2021/01/02.
+//
+
+import UIKit
+import DSMSDK
+
+class ViewController: UIViewController {
+
+    var refreshToken = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginWithDSMAuth(_ sender: Any) {
+        DSMAUTH.loginWithDSMAuth(vc: self) { (token, error) in
+            if let error = error {
+                print(error)
+            } else {
+                print(token!.Access_Token)
+                print(token!.Refresh_Token)
+                self.refreshToken = token!.Refresh_Token
+            }
+        }
+    }
+    
+    @IBAction func tokenRefresh(_ sender: Any){
+        DSMAUTH.tokenRefresh(refresh_token: refreshToken){ (access_token, error) in
+            if let error = error{
+                print(error)
+            } else{
+                print(access_token!)
+            }
+        }
+    }
+    
+    @IBAction func getMyInfo(_ sender: Any) {
+        DSMAUTH.me(access_token: refreshToken){ (user, error) in
+            if let error = error{
+                print(error)
+            } else{
+                print(user!)
+            }
+        }
+    }
+
+
+}
+
